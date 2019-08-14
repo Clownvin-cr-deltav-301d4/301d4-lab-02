5f5c9641e874b653843665a0a234eeb7ca291414
Calvin G Hall <123chall@gmail.com>
2019-08-14 09:40:36 -0500

    created gallery from json

---
+++ js/app.js
@@ -0,0 +1,42 @@
+'use-strict';
+/*
+[
+  {
+    "image_url": "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg",
+    "title": "UniWhal",
+    "description": "A unicorn and a narwhal nuzzling their horns",
+    "keyword": "narwhal",
+    "horns": 1
+  },*/
+
+class Image {
+  constructor(imageJson) {
+    this.url = imageJson.image_url;
+    this.title = imageJson.title;
+    this.description = imageJson.description;
+    this.keyword = imageJson.keyword;
+    this.horns = imageJson.horns;
+    //Element
+    this.element = $('<section class=\'image\'></section>');
+    this.element.append(`<h3>${this.title}</h3>`);
+    this.element.append(`<img id='${this.title}-image' src='${this.url}'></img>`);
+    this.element.append(`<p class'description'>${this.description}</p>`);
+  }
+
+  getElement() {
+    return this.element;
+  }
+}
+
+$(document).ready(() => {
+  const images = [];
+  $.get('./data/page-1.json', (data) => {
+    data = JSON.parse(data);
+    for (const imageJson of data) {
+      images.push(new Image(imageJson));
+    }
+    for (const image of images) {
+      $('main').append(image.getElement());
+    }
+  });
+});
